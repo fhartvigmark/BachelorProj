f92b0c04c1b85e6bb5343e07769e3495d2bfdd3e
Hart <fhartvigmark@gmail.com>
2019-04-24 11:17:59 +0200

    first bfs test

---
+++ test/bfs-test.cpp
@@ -0,0 +1,45 @@
+#include <gtest/gtest.h>
+#include <bfs.h>
+
+struct graph_state {
+    int startnode;
+    int scccolor;
+    std::list<int> scclist;
+    std::list<int> fwlist;
+    std::list<int> bwlist;
+}
+
+struct SimpleReachabilityTest : testing::WithParamInterface<graph_state> {
+
+    struct enhancedgraph* enhgraph;
+
+    SimpleReachabilityTest() {
+        PNGraph graph = TNGraph::New();
+        graph->AddNode(1);
+        graph->AddNode(2);
+        graph->AddNode(3);
+        graph->AddEdge(1, 2);
+        graph->AddEdge(2, 3);
+
+        enhgraph.graph = &graph;
+        TIntH colors;
+        colors(graph->GetNodes());
+        enhgraph.colors = &colors;
+        enhgraph.colorGen = new ColorGenerator();
+    }
+
+    virtual ~SimpleReachabilityTest() {
+        delete enhgraph;
+    }
+}
+
+TEST_P(SimpleReachabilityTest, BasicColors) {
+    auto gs = GetParam();
+    auto newcolors = bfs::colorbfs(enhgraph, gs.scccolor, gs.startnode);
+    EXPECT_EQ(enhgraph->colors->GetDat(gs.startnode), gs.scccolor);
+}
+
+INSTANTIATE_TEST_CASE_P(Default, SimpleReachabilityTest,
+    testing::Values(
+        graph_state{1, 42, (1), (2, 3), ()}
+    ));

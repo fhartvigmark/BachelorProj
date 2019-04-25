56252f857f32c93ded9b000e18f42704d9904843
Eske <eskehoy@gmail.com>
2019-04-25 15:10:43 +0200

    Added enhancedgraph unit tests.

---
+++ test/enhancedgraph-test.cpp
@@ -0,0 +1,77 @@
+#include <gtest/gtest.h>
+#include <core.h>
+
+struct SimpleSetupTest : testing::Test {
+    enhancedgraph* enhgraph = new enhancedgraph;
+
+    SimpleSetupTest() {
+        PNGraph graph = TNGraph::New();
+        graph->AddNode(1);
+        graph->AddNode(2);
+        graph->AddNode(3);
+        graph->AddEdge(1, 2);
+        graph->AddEdge(2, 3);
+
+        enhgraph->graph = &graph;
+        TIntH *colors = new TIntH();
+        enhgraph->colors = colors;
+        enhgraph->colorGen = new ColorGenerator();
+    }
+
+    virtual ~SimpleSetupTest() {
+        delete enhgraph;
+    }
+};
+
+struct MapSetupTest : testing::Test {
+	enhancedgraph* enhgraph = new enhancedgraph;
+
+	MapSetupTest() {
+		PNGraph graph = TNGraph::New();
+        graph->AddNode(1);
+        graph->AddNode(2);
+        graph->AddNode(3);
+        graph->AddEdge(1, 2);
+        graph->AddEdge(2, 3);
+
+        enhgraph->graph = &graph;
+        TIntH *colors = new TIntH();
+        enhgraph->colors = colors;
+
+
+		for (PNGraph::TObj::TNodeI NI = graph->BegNI(); NI < graph->EndNI(); NI++)
+		{
+			colors->AddDat(NI.GetId(), 0);
+		}
+	}
+};
+
+TEST_F(SimpleSetupTest, ColorIncrementEachTime) {
+	int color1 = enhgraph->colorGen->getNext();
+	int color2 = enhgraph->colorGen->getNext();
+
+    EXPECT_EQ(1, color1);
+    EXPECT_EQ(2, color2);
+}
+
+TEST_F(SimpleSetupTest, HashMapStartsEmpty) {
+	TIntH *colorMap = enhgraph->colors;
+    EXPECT_EQ(0, colorMap->Len());
+}
+
+TEST_F(MapSetupTest, MapStartsWithAllNodes) {
+	TIntH *colorMap = enhgraph->colors;
+	EXPECT_EQ(3, colorMap->Len());
+}
+
+TEST_F(MapSetupTest, MapStartsWithZeroValues) {
+	TIntH *colorMap = enhgraph->colors;
+
+	for (THashKeyDatI<TInt, TInt> i = colorMap->BegI(); i < colorMap->EndI(); i++)
+    {
+		EXPECT_EQ(0, i.GetDat());
+    }
+}
+
+
+

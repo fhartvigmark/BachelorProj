16e3bf4e12f8e521c8e98fbf8618c2d764003d60
Hart <fhartvigmark@gmail.com>
2019-04-25 15:11:16 +0200

    Small cycle test

--- test/bfs-test.cpp
+++ test/bfs-test.cpp
@@ -33,11 +33,41 @@ struct SimpleReachabilityTest : testing::Test, testing::WithParamInterface<graph
     }
 };
 
+struct SmallCycleTest : testing::Test, testing::WithParamInterface<graph_state>
+{
+
+	enhancedgraph *enhgraph = new enhancedgraph;
+
+	SmallCycleTest()
+	{
+		PNGraph graph = TNGraph::New();
+		graph->AddNode(1);
+		graph->AddNode(2);
+		graph->AddEdge(1, 2);
+		graph->AddEdge(2, 1);
+
+		enhgraph->graph = &graph;
+		TIntH *colors = new TIntH();
+		enhgraph->colors = colors;
+		enhgraph->colorGen = new ColorGenerator();
+	}
+
+	virtual ~SmallCycleTest()
+	{
+		delete enhgraph;
+	}
+};
+
 TEST_P(SimpleReachabilityTest, BasicColors) {
     auto gs = GetParam();
     auto newcolors = bfs::colorbfs(enhgraph, gs.color, gs.startnode);
-	EXPECT_NE(enhgraph->colors->GetDat(gs.startnode), newcolors.first);
-	EXPECT_NE(enhgraph->colors->GetDat(gs.startnode), newcolors.second);
+
+	for (int sccnode : gs.scclist)
+	{
+		EXPECT_NE(enhgraph->colors->GetDat(sccnode), gs.color);
+		EXPECT_NE(enhgraph->colors->GetDat(sccnode), newcolors.first);
+		EXPECT_NE(enhgraph->colors->GetDat(sccnode), newcolors.second);
+	}
 
 	for (int fwnode: gs.fwlist) {
 		EXPECT_EQ(enhgraph->colors->GetDat(fwnode), newcolors.first);
@@ -48,8 +78,36 @@ TEST_P(SimpleReachabilityTest, BasicColors) {
 	}
 }
 
+TEST_P(SmallCycleTest, BasicCycle)
+{
+	auto gs = GetParam();
+	auto newcolors = bfs::colorbfs(enhgraph, gs.color, gs.startnode);
+
+	for (int sccnode : gs.scclist)
+	{
+		EXPECT_NE(enhgraph->colors->GetDat(sccnode), gs.color);
+		EXPECT_NE(enhgraph->colors->GetDat(sccnode), newcolors.first);
+		EXPECT_NE(enhgraph->colors->GetDat(sccnode), newcolors.second);
+	}
+
+	for (int fwnode : gs.fwlist)
+	{
+		EXPECT_EQ(enhgraph->colors->GetDat(fwnode), newcolors.first);
+	}
+
+	for (int bwnode : gs.bwlist)
+	{
+		EXPECT_EQ(enhgraph->colors->GetDat(bwnode), newcolors.second);
+	}
+}
+
 INSTANTIATE_TEST_CASE_P(Default, SimpleReachabilityTest,
 						testing::Values(
 							graph_state{1, 0, {1}, {2, 3}, {}},
 							graph_state{2, 0, {2}, {3}, {1}},
 							graph_state{3, 0, {3}, {}, {1, 2}}));
+
+INSTANTIATE_TEST_CASE_P(Default, SmallCycleTest,
+						testing::Values(
+							graph_state{1, 0, {1, 2}, {}, {}},
+							graph_state{2, 0, {1, 2}, {}, {}}));

2caa9d76e6be7ebf51652313247c06212d31782f
Hart <fhartvigmark@gmail.com>
2019-04-29 13:21:43 +0200

    basic test for advanced pivots, pass

--- test/pivot-test.cpp
+++ test/pivot-test.cpp
@@ -49,6 +49,42 @@ struct ColorGraphTest : testing::Test, testing::WithParamInterface<pivot_state> 
     }
 };
 
+struct AdvancedColorGraphTest : testing::Test {
+
+	enhancedgraph* enhgraph;
+
+	AdvancedColorGraphTest()
+	{
+		PNGraph graph = TNGraph::New();
+		graph->AddNode(1);
+		graph->AddNode(2);
+		graph->AddNode(3);
+		graph->AddNode(4);
+		graph->AddNode(5);
+		graph->AddNode(6);
+		graph->AddEdge(1, 2);
+		graph->AddEdge(2, 1);
+		graph->AddEdge(3, 2);
+		graph->AddEdge(4, 3);
+		graph->AddEdge(5, 3);
+		graph->AddEdge(6, 3);
+
+		enhgraph = new enhancedgraph(graph);
+
+		enhgraph->colors->AddDat(1, 1);
+		enhgraph->colors->AddDat(2, 1);
+		enhgraph->colors->AddDat(3, 1);
+		enhgraph->colors->AddDat(4, 2);
+		enhgraph->colors->AddDat(5, 2);
+		enhgraph->colors->AddDat(6, 2);
+	}
+
+	virtual ~AdvancedColorGraphTest()
+	{
+		delete enhgraph;
+	}
+};
+
 TEST_P(SimpleGraphTest, CanFindStartnode) {
     auto gs = GetParam();
     int startnode = pivot::getPivot(enhgraph, gs.color);
@@ -85,6 +121,20 @@ TEST_P(ColorGraphTest, FindsCorrectNodeWhenMultipleColors) {
 	}
 }
 
+TEST_F(AdvancedColorGraphTest, FindsMaxDegree)
+{
+	int startnode = pivot::getPivotMaxDegree(enhgraph, 1);
+
+	EXPECT_EQ(startnode, 3);
+}
+
+TEST_F(AdvancedColorGraphTest, IgnoresNodesOfDifferentColor)
+{
+	int startnode = pivot::getPivotMaxDegreeColor(enhgraph, 1);
+
+	EXPECT_EQ(startnode, 2);
+}
+
 INSTANTIATE_TEST_CASE_P(Default, SimpleGraphTest,
 						testing::Values(
 							pivot_state{1, 0},
@@ -95,4 +145,3 @@ INSTANTIATE_TEST_CASE_P(Default, ColorGraphTest,
 							pivot_state{3, 40},
 							pivot_state{1, 2},
 							pivot_state{-1, 39}));
-

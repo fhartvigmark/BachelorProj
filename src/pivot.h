5f529191fdd9e6d164ad0579fcb6c4a4e6c06965
Hart <fhartvigmark@gmail.com>
2019-04-29 11:42:51 +0200

    Simple max degree pivot

--- src/pivot.h
+++ src/pivot.h
@@ -5,4 +5,6 @@ class pivot
 {
   public:
     static int getPivot(enhancedgraph *g, int color);
+    static int getPivotMaxDegree(enhancedgraph *g, int color);
+    static int getPivotMaxDegreeColor(enhancedgraph *g, int color);
 };

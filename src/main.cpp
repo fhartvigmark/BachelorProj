260db96d35a176a75ad52423ed0856e0bdc1b97a
Eske <eskehoy@gmail.com>
2019-05-06 18:06:16 +0200

    Clean up and more debug arguments in main file.

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,27 @@ int main(int argc, char **argv)
 {
     // Input Parameters
     Env = TEnv(argc, argv, TNotify::StdNotify);
-    Env.PrepArgs(TStr::Fmt("Circles. build: %s, %s. Time: %s",
+    Env.PrepArgs(TStr::Fmt("SCC. build: %s, %s. Time: %s",
                            __TIME__, __DATE__, TExeTm::GetCurTm()));
     const TStr InEdges =
-        Env.GetIfArgPrefixStr("-g", "fb1.edges", "Input graph (directed)");
+        Env.GetIfArgPrefixStr("-g ", "fb1.edges", "Input graph (directed)");
     const TInt Trimlevels =
-        Env.GetIfArgPrefixInt("-t", 0, "Input number of trim levels");
+        Env.GetIfArgPrefixInt("-t ", 0, "Input number of trim levels");
     const TInt PivotMethod =
-        Env.GetIfArgPrefixInt("-p", 0, "Pivot selection (0 for random)");
+        Env.GetIfArgPrefixInt("-p ", 0, "Pivot selection (0 for random)");
     const TInt FwBwMethod =
-        Env.GetIfArgPrefixInt("-m", 0, "Specify FW-BW variant");
+        Env.GetIfArgPrefixInt("-m ", 0, "Specify FW-BW variant");
+	const TInt Timer =
+        Env.GetIfArgPrefixInt("-time ", 0, "Time execution");
+	const TInt Output =
+        Env.GetIfArgPrefixInt("-out ", 0, "Print SCC output to file");
 
-    // Load an example graph
+    // Load the graph 
     PNGraph Graph = TSnap::LoadEdgeList<PNGraph>(InEdges);
-    //int n = scc::FindSCCs(Graph, Trimlevels, PivotMethod, FwBwMethod);
-    // Traverse the nodes
-    //for (TNGraph::TNodeI NI = Graph->BegNI(); NI < Graph->EndNI(); NI++)
-    //{
-    //    printf("%d %d %d\n", NI.GetId(), NI.GetOutDeg(), NI.GetInDeg());
-    //}
 
 	scc::FindSCCs(Graph, 0, 0, 0);
+
+	//TODO add printing and timing handler here
 
     return 0;
 }

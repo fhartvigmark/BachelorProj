701d6709642367f0dddbacaf91b58b2fc77ff510
Hart <fhartvigmark@gmail.com>
2019-05-07 14:05:59 +0200

    Single-threaded trim done

--- src/trim.cpp
+++ src/trim.cpp
@@ -1,8 +1,10 @@
 #include "trim.h"
 
+
 int trim::trim1(enhancedgraph *g, int color)
 {
-    TIntH *colors = g->colors;
+	TSnapQueue<int> Queue;
+	TIntH *colors = g->colors;
 	PNGraph graph = g->graph;
 
 	for (TNGraph::TNodeI NI = graph->BegNI(); NI < graph->EndNI(); NI++)
@@ -25,6 +27,7 @@ int trim::trim1(enhancedgraph *g, int color)
 
 			if (inDegree == 0) {
 				colors->AddDat(node, g->colorGen->getNext());
+				Queue.Push(node);
 				continue;
 			}
 
@@ -43,11 +46,175 @@ int trim::trim1(enhancedgraph *g, int color)
 
 			if (outDegree == 0) {
 				colors->AddDat(node, g->colorGen->getNext());
+				Queue.Push(node);
 				continue;
 			}
 		}
         
     }
-    
+	while (!Queue.Empty())
+	{
+		int oldnode = Queue.Top();
+		Queue.Pop();
+
+		TNGraph::TNodeI OldNodeI = graph->GetNI(oldnode);
+		int i;
+		for (i = 0; i < OldNodeI.GetInDeg(); i++)
+		{
+			const int node = OldNodeI.GetInNId(i);
+			if (colors->GetDat(node) == color)
+			{
+				int inDegree = 0;
+				TNGraph::TNodeI NodeI = graph->GetNI(node);
+
+				int v = 0;
+				for (v = 0; v < NodeI.GetInDeg(); v++)
+				{
+					const int outNode = NodeI.GetInNId(v);
+
+					if (colors->GetDat(outNode) == color && outNode != node)
+					{
+						inDegree = 1;
+						break;
+					}
+				}
+
+				if (inDegree == 0)
+				{
+					colors->AddDat(node, g->colorGen->getNext());
+					Queue.Push(node);
+					continue;
+				}
+
+				int outDegree = 0;
+				NodeI = graph->GetNI(node);
+
+				for (v = 0; v < NodeI.GetOutDeg(); v++)
+				{
+					const int outNode = NodeI.GetOutNId(v);
+
+					if (colors->GetDat(outNode) == color && outNode != node)
+					{
+						outDegree = 1;
+						break;
+					}
+				}
+
+				if (outDegree == 0)
+				{
+					colors->AddDat(node, g->colorGen->getNext());
+					Queue.Push(node);
+					continue;
+				}
+			}
+		}
+		for (i = 0; i < OldNodeI.GetOutDeg(); i++)
+		{
+			const int node = OldNodeI.GetOutNId(i);
+			if (colors->GetDat(node) == color)
+			{
+				int inDegree = 0;
+				TNGraph::TNodeI NodeI = graph->GetNI(node);
+
+				int v = 0;
+				for (v = 0; v < NodeI.GetInDeg(); v++)
+				{
+					const int outNode = NodeI.GetInNId(v);
+
+					if (colors->GetDat(outNode) == color && outNode != node)
+					{
+						inDegree = 1;
+						break;
+					}
+				}
+
+				if (inDegree == 0)
+				{
+					colors->AddDat(node, g->colorGen->getNext());
+					Queue.Push(node);
+					continue;
+				}
+
+				int outDegree = 0;
+				NodeI = graph->GetNI(node);
+
+				for (v = 0; v < NodeI.GetOutDeg(); v++)
+				{
+					const int outNode = NodeI.GetOutNId(v);
+
+					if (colors->GetDat(outNode) == color && outNode != node)
+					{
+						outDegree = 1;
+						break;
+					}
+				}
+
+				if (outDegree == 0)
+				{
+					colors->AddDat(node, g->colorGen->getNext());
+					Queue.Push(node);
+					continue;
+				}
+			}
+		}
+	}
+	
     return -1;
+};
+
+int trim::partrim1(enhancedgraph *g, int color)
+{
+	TIntH *colors = g->colors;
+	PNGraph graph = g->graph;
+
+	#pragma parallel for
+	for (TNGraph::TNodeI NI = graph->BegNI(); NI < graph->EndNI(); NI++)
+	{
+		int node = NI.GetId();
+		if (colors->GetDat(node) == color)
+		{
+			int inDegree = 0;
+			TNGraph::TNodeI NodeI = graph->GetNI(node);
+
+			int v = 0;
+			for (v = 0; v < NodeI.GetInDeg(); v++)
+			{
+				const int outNode = NodeI.GetInNId(v);
+
+				if (colors->GetDat(outNode) == color && outNode != node)
+					{
+					inDegree = 1;
+					break;
+				}
+			}
+
+			if (inDegree == 0)
+			{
+				colors->AddDat(node, g->colorGen->getNext());
+				continue;
+			}
+
+			int outDegree = 0;
+			NodeI = graph->GetNI(node);
+
+			for (v = 0; v < NodeI.GetOutDeg(); v++)
+			{
+				const int outNode = NodeI.GetOutNId(v);
+
+				if (colors->GetDat(outNode) == color && outNode != node)
+				{
+					outDegree = 1;
+					break;
+				}
+			}
+
+			if (outDegree == 0)
+			{
+				colors->AddDat(node, g->colorGen->getNext());
+				continue;
+			}
+		}
+	}
+
+	return -1;
 };
--- test/trim-test.cpp
+++ test/trim-test.cpp
@@ -33,6 +33,33 @@ struct SimpleGraphTest : testing::Test {
     }
 };
 
+struct SimpleChainTest :testing::Test {
+
+	enhancedgraph *enhgraph;
+
+	SimpleChainTest() {
+		PNGraph graph = TNGraph::New();
+		graph->AddNode(1);
+		graph->AddNode(2);
+		graph->AddNode(3);
+		graph->AddEdge(1, 2);
+		graph->AddEdge(2, 3);
+
+		graph->AddNode(4);
+		graph->AddNode(5);
+		graph->AddNode(6);
+		graph->AddEdge(6, 5);
+		graph->AddEdge(5, 4);
+
+		enhgraph = new enhancedgraph(graph);
+	}
+
+	virtual ~SimpleChainTest() {
+		delete enhgraph;
+	}
+
+};
+
 struct ColorGraphTest : testing::Test {
 
     enhancedgraph* enhgraph;
@@ -84,7 +111,8 @@ TEST_F(SimpleGraphTest, TrimFindsSCCs) {
 	EXPECT_NE(0, colors->GetDat(6));
 
 	EXPECT_NE(colors->GetDat(4), colors->GetDat(5));
-	EXPECT_NE(colors->GetDat(5), colors->GetDat(6));	
+	EXPECT_NE(colors->GetDat(5), colors->GetDat(6));
+	EXPECT_NE(colors->GetDat(4), colors->GetDat(6));
 }
 
 TEST_F(SimpleGraphTest, TrimRespectsColors) {
@@ -141,5 +169,37 @@ TEST_F(ColorGraphTest, TrimFindsSCCs) {
 
 	EXPECT_NE(0, colors->GetDat(5));	
 	EXPECT_NE(2, colors->GetDat(5));	
+}
+
+TEST_F(SimpleChainTest, TrimRunsMultiplePasses) {
+	TIntH *colors = enhgraph->colors;
+	trim::trim1(enhgraph, 0);
+
+	EXPECT_NE(0, colors->GetDat(1));
+	EXPECT_NE(0, colors->GetDat(2));
+	EXPECT_NE(0, colors->GetDat(3));
+	EXPECT_NE(0, colors->GetDat(4));
+	EXPECT_NE(0, colors->GetDat(5));
+	EXPECT_NE(0, colors->GetDat(6));
+
+	EXPECT_NE(colors->GetDat(1), colors->GetDat(2));
+	EXPECT_NE(colors->GetDat(1), colors->GetDat(3));
+	EXPECT_NE(colors->GetDat(1), colors->GetDat(4));
+	EXPECT_NE(colors->GetDat(1), colors->GetDat(5));
+	EXPECT_NE(colors->GetDat(1), colors->GetDat(6));
+
+	EXPECT_NE(colors->GetDat(2), colors->GetDat(3));
+	EXPECT_NE(colors->GetDat(2), colors->GetDat(4));
+	EXPECT_NE(colors->GetDat(2), colors->GetDat(5));
+	EXPECT_NE(colors->GetDat(2), colors->GetDat(6));
+
+	EXPECT_NE(colors->GetDat(3), colors->GetDat(4));
+	EXPECT_NE(colors->GetDat(3), colors->GetDat(5));
+	EXPECT_NE(colors->GetDat(3), colors->GetDat(6));
+
+	EXPECT_NE(colors->GetDat(4), colors->GetDat(5));
+	EXPECT_NE(colors->GetDat(4), colors->GetDat(6));
+
+	EXPECT_NE(colors->GetDat(5), colors->GetDat(6));
 }
 

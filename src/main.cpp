c73d1395aab1429491f1f7ee5734b6982704b24a
Eske <eskehoy@gmail.com>
2019-05-07 23:19:21 +0200

    Added bootstrapper to enable OMP_CANCELLATION environment variable.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,20 @@
 
 int main(int argc, char **argv)
 {
+	//https://stackoverflow.com/questions/46482468/enable-cancellation-of-openmp-threads-from-inside-program
+	char *hasCancel = getenv("OMP_CANCELLATION");
+	if (hasCancel == nullptr) {
+		printf("Bootstrapping...");
+		setenv("OMP_CANCELLATION", "true", 1);
+		// Restart the program here
+		int output = execvp(argv[0], argv);
+		// Execution should not continue past here
+		printf("Bootstrapping failed with code %d\n",output);
+		exit(1);
+	} else {
+		puts("Bootstrapping complete");
+	}
+
     // Input Parameters
     Env = TEnv(argc, argv, TNotify::StdNotify);
     Env.PrepArgs(TStr::Fmt("SCC. build: %s, %s. Time: %s",
--- test/runAll.cpp
+++ test/runAll.cpp
@@ -1,6 +1,20 @@
 #include <gtest/gtest.h>
 
 int main(int argc, char **argv) {
+	//https://stackoverflow.com/questions/46482468/enable-cancellation-of-openmp-threads-from-inside-program
+	char *hasCancel = getenv("OMP_CANCELLATION");
+	if (hasCancel == nullptr) {
+		printf("Bootstrapping...");
+		setenv("OMP_CANCELLATION", "true", 1);
+		// Restart the program here
+		int output = execvp(argv[0], argv);
+		// Execution should not continue past here
+		printf("Bootstrapping failed with code %d\n",output);
+		exit(1);
+	} else {
+		puts("Bootstrapping complete");
+	}
+
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }

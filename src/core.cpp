0f85bdfdf884758cc5352d2e2db08562c9d8884b
Eske <eskehoy@gmail.com>
2019-05-20 12:02:25 +0200

    Added timer functionality.

--- src/core.cpp
+++ src/core.cpp
@@ -4,6 +4,50 @@ ColorGenerator::ColorGenerator() {
 	lastColor = 0;
 }
 
+void enhancedgraph::endTimer(std::chrono::high_resolution_clock::time_point start, eTimer timer) {
+	if (TIMER_ENABLED)
+	{
+		auto end = std::chrono::high_resolution_clock::now();
+		auto dur = end - start;
+
+		switch (timer)
+		{
+			case eTimer::MAIN:
+				omp_set_lock(&lMain);
+				tMain += dur;
+				omp_unset_lock(&lMain);
+				break;
+			case eTimer::FirstFWBW:
+				omp_set_lock(&lFirstFWBW);
+				tFirstFWBW += dur;
+				omp_unset_lock(&lFirstFWBW);				
+				break;
+			case eTimer::FWBWs:
+				omp_set_lock(&lFWBW);
+				tFWBW += dur;
+				omp_unset_lock(&lFWBW);
+				break;
+			case eTimer::TRIM:
+				omp_set_lock(&lTrim);
+				tTrim += dur;
+				omp_unset_lock(&lTrim);
+				break;
+			case eTimer::PIVOT:
+				omp_set_lock(&lPivot);
+				tPivot += dur;
+				omp_unset_lock(&lPivot);
+				break;
+			case eTimer::SETUP:
+				omp_set_lock(&lSetup);
+				tSetup += dur;
+				omp_unset_lock(&lSetup);
+				break;
+			default:
+				break;
+		}
+	}
+}
+
 enhancedgraph::enhancedgraph(PNGraph g) : TIMER_ENABLED(false), RAND_WALK_ITERATIONS(10){
 	graph = g;
 	colorGen = new ColorGenerator();
@@ -20,9 +64,39 @@ enhancedgraph::enhancedgraph(PNGraph g) : TIMER_ENABLED(false), RAND_WALK_ITERAT
 
 		i++;
 	}
+
+	if (TIMER_ENABLED) {
+		tMain = std::chrono::high_resolution_clock::duration::zero();
+		tFirstFWBW = std::chrono::high_resolution_clock::duration::zero();
+		tFWBW = std::chrono::high_resolution_clock::duration::zero();
+		tTrim = std::chrono::high_resolution_clock::duration::zero();
+		tPivot = std::chrono::high_resolution_clock::duration::zero();
+		tSetup = std::chrono::high_resolution_clock::duration::zero();
+
+		omp_init_lock(&lMain);
+		omp_init_lock(&lFirstFWBW);
+		omp_init_lock(&lFWBW);
+		omp_init_lock(&lTrim);
+		omp_init_lock(&lPivot);
+		omp_init_lock(&lSetup);
+	}
 }
 
 //TODO: make params for consts, also args in main
 enhancedgraph::enhancedgraph() : TIMER_ENABLED(false), RAND_WALK_ITERATIONS(10){
-	
+	if (TIMER_ENABLED) {
+		tMain = std::chrono::high_resolution_clock::duration::zero();
+		tFirstFWBW = std::chrono::high_resolution_clock::duration::zero();
+		tFWBW = std::chrono::high_resolution_clock::duration::zero();
+		tTrim = std::chrono::high_resolution_clock::duration::zero();
+		tPivot = std::chrono::high_resolution_clock::duration::zero();
+		tSetup = std::chrono::high_resolution_clock::duration::zero();
+
+		omp_init_lock(&lMain);
+		omp_init_lock(&lFirstFWBW);
+		omp_init_lock(&lFWBW);
+		omp_init_lock(&lTrim);
+		omp_init_lock(&lPivot);
+		omp_init_lock(&lSetup);
+	}
 }
--- src/core.h
+++ src/core.h
@@ -6,7 +6,9 @@
 enum ePivot{Random, Max, MaxColor, ParRandom, ParMax, ParMaxColor, RandWalk};
 enum eMethod{FWBW, ParFWBW, RecFWBW};
 enum eTrim{Trim1, Trim2, Trim3, ParTrim1, ParTrim2, ParTrim3};
-enum eTimer{Main, FirstFWBW, FWBWs, TRIM, PIVOT, SETUP};
+enum eTimer{MAIN, FirstFWBW, FWBWs, TRIM, PIVOT, SETUP};
+
+//typedef std::duration<unsigned long long> my_duration;
 
 class ColorGenerator
 {
@@ -33,13 +35,21 @@ class enhancedgraph
 		//TODO: test
 		void endTimer(std::chrono::high_resolution_clock::time_point start, eTimer timer);
 
+		//TODO: add deconstructor??
 		enhancedgraph(PNGraph g);
 		enhancedgraph();
 	private:
-        std::atomic<long> tMain;
-        std::atomic<long> tFirstFWBW;
-        std::atomic<long> tFWBW;
-        std::atomic<long> tTrim;
-        std::atomic<long> tPivot;
-        std::atomic<long> tSetup;
+        std::chrono::high_resolution_clock::duration tMain;
+        std::chrono::high_resolution_clock::duration tFirstFWBW;
+        std::chrono::high_resolution_clock::duration tFWBW;
+        std::chrono::high_resolution_clock::duration tTrim;
+        std::chrono::high_resolution_clock::duration tPivot;
+        std::chrono::high_resolution_clock::duration tSetup;
+
+		omp_lock_t lMain;
+        omp_lock_t lFirstFWBW;
+        omp_lock_t lFWBW;
+        omp_lock_t lTrim;
+        omp_lock_t lPivot;
+        omp_lock_t lSetup;
 };

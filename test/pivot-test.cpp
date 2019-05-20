d67023611be31b84e73992b37494073251df8288
Eske <eskehoy@gmail.com>
2019-05-20 12:35:40 +0200

    Minor change in pivot test.

--- test/pivot-test.cpp
+++ test/pivot-test.cpp
@@ -120,7 +120,7 @@ TEST_P(SimpleGraphTest, CanFindStartnode) {
 		}
 	} else if (gs.method == 3) {
 		EXPECT_TRUE(enhgraph->graph->IsNode(startnode));
-		EXPECT_TRUE(startnode < omp_get_max_threads());
+		EXPECT_TRUE(startnode <= omp_get_max_threads());
 		EXPECT_EQ(gs.color, enhgraph->colors->GetDat(startnode));
 	} else if (gs.method == 6 || gs.method == 7) {
 		EXPECT_TRUE(enhgraph->graph->IsNode(startnode));

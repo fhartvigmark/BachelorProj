264063a05332ff8125ae61e1187bec125d65ce7e
Eske <eskehoy@gmail.com>
2019-05-22 22:05:30 +0200

    IDFK...

--- src/fwbw.cpp
+++ src/fwbw.cpp
@@ -5,7 +5,11 @@ int fwbw::FWBW(enhancedgraph *g, int trimlevel, int pivotmethod, int startColor,
         case 0:
             return fwbw::basicFWBW(g, trimlevel, pivotmethod, startColor);
 		case 1:
-            return fwbw::parFWBW(g, trimlevel, pivotmethod, startColor);			
+            return fwbw::parFWBW(g, trimlevel, pivotmethod, startColor);
+		case 2:
+			return fwbw::recFWBW(g, trimlevel, pivotmethod, startColor);
+		default:
+			return -1;
     }
 }
 
@@ -27,10 +31,11 @@ int fwbw::basicFWBW(enhancedgraph *g, int trimlevel, int pivotmethod, int startC
     basicFWBW(g, trimlevel, pivotmethod, startColor);
     basicFWBW(g, trimlevel, pivotmethod, newColors.first);
     basicFWBW(g, trimlevel, pivotmethod, newColors.second);
-    return 0;
-};
 
-//Coloring based basic fw-bw
+    return 0;
+}
+
+//Coloring based parallel fw-bw
 int fwbw::parFWBW(enhancedgraph *g, int trimlevel, int pivotmethod, int startColor)
 {
 	//Find pivot node
@@ -49,5 +54,47 @@ int fwbw::parFWBW(enhancedgraph *g, int trimlevel, int pivotmethod, int startCol
 	parFWBW(g, trimlevel, pivotmethod, startColor);
 	parFWBW(g, trimlevel, pivotmethod, newColors.first);
 	parFWBW(g, trimlevel, pivotmethod, newColors.second);
+	
 	return 0;
-};
+}
+
+//Coloring based recursive fw-bw
+int fwbw::recFWBW(enhancedgraph *g, int trimlevel, int pivotmethod, int startColor)
+{
+	//Find pivot node
+	int startNode = pivot::findPivot(g, startColor, pivotmethod);
+	if (startNode == -1)
+	{
+		return -1;
+	}
+
+	trim::doParTrim(trimlevel, g, startColor);
+	//First: fwColor, Second: bwColor
+	TimePoint start = g->startTimer();
+	std::pair<int, int> newColors = bfs::colorbfs(g, startColor, startNode);
+	g->endTimer(start, eTimer::FWBWs);
+
+	#pragma omp parallel
+	{
+		#pragma omp single nowait
+		{
+			#pragma omp task shared(g)
+			{
+				recFWBW(g, trimlevel, pivotmethod, startColor);
+			}
+
+			#pragma omp task shared(g)
+			{
+				recFWBW(g, trimlevel, pivotmethod, newColors.first);
+			}
+
+			#pragma omp task shared(g)
+			{
+				recFWBW(g, trimlevel, pivotmethod, newColors.second);
+			}
+		}
+		#pragma omp taskwait
+	}
+	
+	return 0;
+}
--- test/fwbw-test.cpp
+++ test/fwbw-test.cpp
@@ -124,8 +124,81 @@ TEST_F(Graph1Test, ParSCCCheck)
 	EXPECT_NE(scc4, 0);
 }
 
+TEST_F(Graph1Test, RecSCCCheck)
+{
+	int status = fwbw::FWBW(enhgraph, 0, 0, 0, 2);
+	//No error
+	EXPECT_EQ(status, 0);
+	int scc1_1 = enhgraph->colors->GetDat(1);
+	int scc2_1 = enhgraph->colors->GetDat(2);
+	int scc1_2 = enhgraph->colors->GetDat(3);
+	int scc1_3 = enhgraph->colors->GetDat(4);
+	int scc2_2 = enhgraph->colors->GetDat(5);
+	int scc3 = enhgraph->colors->GetDat(6);
+	int scc4 = enhgraph->colors->GetDat(7);
+
+	//Two SCCs found
+	EXPECT_NE(scc1_1, scc2_1);
+	EXPECT_NE(scc1_1, scc3);
+	EXPECT_NE(scc1_1, scc4);
+	EXPECT_NE(scc2_1, scc3);
+	EXPECT_NE(scc2_1, scc4);
+	EXPECT_NE(scc3, scc4);
+
+	//SCCs contain correct nodes
+	EXPECT_EQ(scc1_1, scc1_2);
+	EXPECT_EQ(scc1_1, scc1_3);
+	EXPECT_EQ(scc2_1, scc2_2);
+
+	//SCCs have correct color
+	EXPECT_NE(scc1_1, 0);
+	EXPECT_NE(scc2_1, 0);
+	EXPECT_NE(scc3, 0);
+	EXPECT_NE(scc4, 0);
+}
+
 TEST_F(BasicFWBWTest, ColorCheck){
 	int status = fwbw::FWBW(enhgraph, 0, 0, 0, 0);
+	//No error
+	EXPECT_EQ(status, 0);
+	int scc1 = enhgraph->colors->GetDat(1);
+	int scc2 = enhgraph->colors->GetDat(3);
+
+	//Two SCCs found
+	EXPECT_NE(scc1, scc2);
+
+	//SCCs contain correct nodes
+	EXPECT_EQ(enhgraph->colors->GetDat(2), scc1);
+	EXPECT_EQ(enhgraph->colors->GetDat(4), scc2);
+
+	//SCCs have correct color
+	EXPECT_NE(scc1, 0);
+	EXPECT_NE(scc2, 0);
+}
+
+TEST_F(BasicFWBWTest, ParColorCheck)
+{
+	int status = fwbw::FWBW(enhgraph, 0, 0, 0, 1);
+	//No error
+	EXPECT_EQ(status, 0);
+	int scc1 = enhgraph->colors->GetDat(1);
+	int scc2 = enhgraph->colors->GetDat(3);
+
+	//Two SCCs found
+	EXPECT_NE(scc1, scc2);
+
+	//SCCs contain correct nodes
+	EXPECT_EQ(enhgraph->colors->GetDat(2), scc1);
+	EXPECT_EQ(enhgraph->colors->GetDat(4), scc2);
+
+	//SCCs have correct color
+	EXPECT_NE(scc1, 0);
+	EXPECT_NE(scc2, 0);
+}
+
+TEST_F(BasicFWBWTest, RecColorCheck)
+{
+	int status = fwbw::FWBW(enhgraph, 0, 0, 0, 2);
 	//No error
 	EXPECT_EQ(status, 0);
 	int scc1 = enhgraph->colors->GetDat(1);

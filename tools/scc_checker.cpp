bfdbb09b01b08b51fcf1dd519030036be2654b87
Hart <fhartvigmark@gmail.com>
2019-05-23 23:13:06 +0200

    Output checker almost finalized

---
+++ tools/scc_checker.cpp
@@ -0,0 +1,83 @@
+#include <string>
+#include <iostream>
+#include <map>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+vector<int> generateSCClist(string filename)
+{
+	string line;
+	ifstream sccFile(filename);
+	map<int, int> sccMap;
+
+	if (sccFile.is_open())
+	{
+		while (getline(sccFile, line))
+		{
+			string sccString;
+			stringstream ss(line);
+
+			ss >> sccString;
+			ss >> sccString;
+
+			int sccID = stoi(sccString);
+			sccMap[sccID]++;
+		}
+		sccFile.close();
+	}
+	else
+		cout << "Unable to open file";
+
+	vector<int> sccSizes(sccMap.size());
+
+	for_each(sccMap.begin(), sccMap.end(),
+			 [&sccSizes](const std::pair<int, int> &entry) {
+				 sccSizes.push_back(entry.second);
+			 });
+
+	sort(sccSizes.begin(), sccSizes.end());
+	
+	cout << filename << " contains " << sccSizes.size() << " SCCs \n";
+	for (int i = 0; i < sccSizes.size(); i++)
+	{
+		cout << "SCC " << i << " has size " << sccSizes.at(i) << "\n";
+	}
+	
+	return sccSizes;
+}
+
+int main(int argc, char **argv)
+{
+	string scc1FileName = "";
+	string scc2FileName = "";
+	if (argc == 3)
+	{
+		scc1FileName = argv[1];
+		scc2FileName = argv[2];
+	}
+	else
+	{
+		cout << "Usage: ./scc_checker scc1File scc2File\n";
+		return 1;
+	}
+
+	vector<int> scclist1 = generateSCClist(scc1FileName);
+	vector<int> scclist2 = generateSCClist(scc2FileName);
+
+	if(scclist1.size()!=scclist2.size()){
+		cout << "SCCs have different sizes";
+		return 0;
+	}
+
+	for (int i = 0; i < scclist1.size(); i++)
+	{
+		if(scclist1.at(i)!=scclist2.at(i)){
+			cout << "Size of SCC " << i << " doesn't match " << scclist1.at(i) << " vs. " << scclist2.at(i) << "\n";
+		}
+	}
+
+	return 0;
+}

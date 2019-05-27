53d81cc80e291712549f83765fe35ab8e633b0d7
Eske <eskehoy@gmail.com>
2019-05-27 17:31:45 +0200

    Added new parallel bfs variant.

--- src/bfs.cpp
+++ src/bfs.cpp
@@ -210,6 +210,255 @@ std::pair<int, int> bfs::parbfs(enhancedgraph *g, int color, int startNode) {
 	return std::make_pair(fwColor, bwColor);
 }
 
+std::pair<int, int> bfs::relaxedSearch(enhancedgraph *g, int color, int startNode) {
+	TIntH *colors = g->colors;
+	PNGraph pgraph = g->graph;
+	int totalCount = 0;
+	int count = 0;
+	int threads = omp_get_max_threads();
+	//std::cout << "Threads " << threads << "\n";
+
+	const int sccColor = g->colorGen->getNext();
+	const int fwColor = g->colorGen->getNext();
+	const int bwColor = g->colorGen->getNext();
+
+	TSnapQueue<int> Queue;
+	Queue.Push(startNode);
+	int v = 0;
+
+	//Find FW start nodes
+    while(!Queue.Empty())
+    {
+        const int node = Queue.Top();
+        Queue.Pop();
+
+        if (colors->GetDat(node) == color) {
+            colors->AddDat(node, fwColor);
+
+
+            //Get node iterator for the current node
+            const TNGraph::TNodeI NodeI = pgraph->GetNI(node);
+
+			//Add all out edges that have not already been visited to the queue
+            for (v = 0; v < NodeI.GetOutDeg(); v++) 
+            {
+                const int outNode = NodeI.GetOutNId(v);
+
+                if (colors->GetDat(outNode) == color) {
+                    Queue.Push(outNode);
+                }
+            }
+        }
+
+		if (Queue.Len() >= threads) {
+			break;
+		}
+    }
+
+	//std::cout << "Queue " << Queue.Len() << "\n";
+
+	//Find FW closure
+	if (!Queue.Empty()) {
+		#pragma omp parallel
+		{
+			TSnapQueue<int> *myQueue;
+			int threadNode;
+			int id = omp_get_thread_num();
+
+			if (id == 0) {
+				myQueue = &Queue;
+			} else {
+				myQueue = new TSnapQueue<int>();
+			}
+
+			#pragma omp critical
+			{
+				threadNode = Queue.Top();
+				Queue.Pop();
+			}
+			
+			
+			#pragma omp barrier
+			myQueue->Push(threadNode);
+
+
+			int v = 0;
+			while(!myQueue->Empty())
+			{
+				int node = myQueue->Top();
+				myQueue->Pop();
+
+				if (colors->GetDat(node) == color) {
+					colors->AddDat(node, fwColor);
+
+					//Get node iterator for the current node
+					const TNGraph::TNodeI NodeI = pgraph->GetNI(node);
+
+					//Add all out edges that have not already been visited to the queue
+					for (v = 0; v < NodeI.GetOutDeg(); v++) 
+					{
+						const int outNode = NodeI.GetOutNId(v);
+
+						if (colors->GetDat(outNode) == color)
+						{
+							myQueue->Push(outNode);
+						}
+					}
+				}
+			}
+
+			if (id != 0) {
+				delete myQueue;
+			}
+		}
+	}
+	
+
+	//Something wrong happened
+	if (!Queue.Empty()) {
+		throw "relaxedSearch: FW queue not emptied";
+	}
+
+	//Find BW start nodes
+	Queue.Push(startNode);
+    while (!Queue.Empty())
+    {
+        const int node = Queue.Top();
+        Queue.Pop();
+        const int nodeColor = colors->GetDat(node);
+
+        if (nodeColor == color)
+        {
+            colors->AddDat(node, bwColor);
+
+            //Get node iterator for the current node
+            const TNGraph::TNodeI NodeI = pgraph->GetNI(node);
+
+            //Add all out edges that have not already been visited to the queue
+            for (v = 0; v < NodeI.GetInDeg(); v++)
+            {
+                const int inNode = NodeI.GetInNId(v);
+                const int inNodeColor = colors->GetDat(inNode);
+
+                if ((inNodeColor == color)||(inNodeColor == fwColor))
+                {
+                    Queue.Push(inNode);
+                }
+            }
+        }else if (nodeColor == fwColor)
+        {
+            colors->AddDat(node, sccColor);
+			totalCount++;
+
+            //Get node iterator for the current node
+            const TNGraph::TNodeI NodeI = pgraph->GetNI(node);
+
+            //Add all out edges that have not already been visited to the queue
+            for (v = 0; v < NodeI.GetInDeg(); v++)
+            {
+                const int inNode = NodeI.GetInNId(v);
+                const int inNodeColor = colors->GetDat(inNode);
+
+                if ((inNodeColor == color) || (inNodeColor == fwColor))
+                {
+                    Queue.Push(inNode);
+                }
+            }
+        }
+
+		if (Queue.Len() >= threads) {
+			break;
+		}
+    }
+	
+	//Find BW closure
+	if (!Queue.Empty()) {
+		#pragma omp parallel reduction(+:count)
+		{
+			TSnapQueue<int> *myQueue;
+			int threadNode;
+			int id = omp_get_thread_num();
+
+			if (id == 0) {
+				myQueue = &Queue;
+			} else {
+				myQueue = new TSnapQueue<int>();
+			}
+
+			#pragma omp critical
+			{
+				threadNode = Queue.Top();
+				Queue.Pop();
+			}
+			
+			
+			#pragma omp barrier
+			myQueue->Push(threadNode);
+
+
+			int v = 0;
+			while (!myQueue->Empty())
+			{
+				int node = myQueue->Top();
+				myQueue->Pop();
+				int nodeColor = colors->GetDat(node);
+
+				if (nodeColor == color)
+				{
+					colors->AddDat(node, bwColor);
+
+					//Get node iterator for the current node
+					const TNGraph::TNodeI NodeI = pgraph->GetNI(node);
+
+					//Add all out edges that have not already been visited to the queue
+					for (v = 0; v < NodeI.GetInDeg(); v++)
+					{
+						const int inNode = NodeI.GetInNId(v);
+						const int inNodeColor = colors->GetDat(inNode);
+
+						if ((inNodeColor == color) || (inNodeColor == fwColor))
+						{
+							myQueue->Push(inNode);
+						}
+					}
+				}else if (nodeColor == fwColor)
+				{
+					colors->AddDat(node, sccColor);
+					count++;
+
+					//Get node iterator for the current node
+					const TNGraph::TNodeI NodeI = pgraph->GetNI(node);
+
+					//Add all out edges that have not already been visited to the queue
+					for (v = 0; v < NodeI.GetInDeg(); v++)
+					{
+						const int inNode = NodeI.GetInNId(v);
+						const int inNodeColor = colors->GetDat(inNode);
+
+						if ((inNodeColor == color) || (inNodeColor == fwColor))
+						{
+							myQueue->Push(inNode);
+						}
+					}
+				}
+			}
+
+			if (id != 0) {
+				delete myQueue;
+			}
+		}
+	}
+	
+
+	//Something wrong happened
+	if (!Queue.Empty()) {
+		throw "relaxedSearch: BW queue not emptied";
+	}
+
+	g->reportBFS(color, totalCount + count);
+	return std::make_pair(fwColor, bwColor);
+}
+
 std::pair<int, int> bfs::randomRelaxedSearch(enhancedgraph *g, int color, int startNode) {
 	TIntH *colors = g->colors;
 	PNGraph pgraph = g->graph;
--- src/bfs.h
+++ src/bfs.h
@@ -7,6 +7,7 @@ class bfs
 	public:
 		static std::pair<int, int> colorbfs(enhancedgraph *g, int color, int startNode);
 		static std::pair<int, int> parbfs(enhancedgraph *g, int color, int startNode);
+		static std::pair<int, int> relaxedSearch(enhancedgraph *g, int color, int startNode);
 		static std::pair<int, int> randomRelaxedSearch(enhancedgraph *g, int color, int startNode);
 		static int fwbfs(enhancedgraph *g, int color, int startNode);
 		static int parfwbfs(enhancedgraph *g, int color, int startNode);

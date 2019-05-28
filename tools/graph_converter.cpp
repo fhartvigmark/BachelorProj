5cee45c3d386f39d43316e9870a7d39f0b0d2b25
Eske <eskehoy@gmail.com>
2019-05-28 22:40:19 +0200

    Added graph converter tool.

---
+++ tools/graph_converter.cpp
@@ -0,0 +1,74 @@
+#include <string>
+#include <iostream>
+#include <map>
+#include <fstream>
+#include <iterator>
+#include <sstream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int retVal = 0;
+	string inputFileName = "";
+	string outputFileName = "";
+	if (argc == 3)
+	{
+		inputFileName = argv[1];
+		outputFileName = argv[2];
+	}
+	else
+	{
+		cout << "Usage: ./scc_checker scc1File scc2File \n";
+		return 1;
+	}
+
+	ofstream outfile;
+	outfile.open(outputFileName);
+
+	if (!outfile.is_open()) {
+		cout << "Unable to open output file\n";
+		return 1;
+	}
+
+	string line;
+	ifstream infile(inputFileName);
+	map<int, int> nodes;
+	int counter = 0;
+
+	if (!infile.is_open()) {
+		cout << "Unable to open input file\n";
+		return 1;
+	}
+
+	while (getline(infile, line)) {
+		if (line.at(0) == '#') {
+			outfile << line;
+			continue;
+		}
+
+		int from;
+		int to;
+		istringstream iss(line);
+
+		iss >> from;
+		iss >> to;
+
+		if (nodes.count(from) == 0) {
+			nodes[from] = counter;
+			counter++;
+		}
+
+		if (nodes.count(to) == 0) {
+			nodes[to] = counter;
+			counter++;
+		}
+
+		outfile << nodes.at(from) << " " << nodes.at(to) << "\n";
+
+	}
+
+	infile.close();
+	outfile.close();
+
+	return retVal;
+}

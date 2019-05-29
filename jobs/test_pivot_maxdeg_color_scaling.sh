dc12421f67948378cc457f58d60f657e7550e6e3
Eske <eskehoy@gmail.com>
2019-05-29 23:58:44 +0200

    Removed -out= arg from pivot tests as first iteration output has been verified.

--- jobs/test_pivot_maxdeg_color_scaling.sh
+++ jobs/test_pivot_maxdeg_color_scaling.sh
@@ -4,7 +4,7 @@ for i in 1 2 4 8 16
 do
 	for j in 1 2 3 4 5 6 7 8 9 10
 	do
-		./main -g=Slashdot0902.txt -p=2 -w=${i} -time=3 -a=3 -out= -suffix=PivotMaxDeg_Col_C${i}_iteration${j} -m=1
+		./main -g=Slashdot0902.txt -p=2 -w=${i} -time=3 -a=3 -suffix=PivotMaxDeg_Col_C${i}_iteration${j} -m=1
 	done
 	cat Slashdot0902PivotMaxDeg_Col_C${i}_iteration*.time > Slashdot0902PivotMaxDeg_Col_C${i}.time
 	rm Slashdot0902PivotMaxDeg_Col_C${i}_iteration*.time
--- jobs/test_pivot_random_scaling.sh
+++ jobs/test_pivot_random_scaling.sh
@@ -4,7 +4,7 @@ for i in 1 2 4 8 16
 do
 	for j in 1 2 3 4 5 6 7 8 9 10
 	do
-		./main -g=Slashdot0902.txt -p=0 -w=${i} -time=3 -a=3 -out= -suffix=PivotRandom_C${i}_iteration${j} -m=1
+		./main -g=Slashdot0902.txt -p=0 -w=${i} -time=3 -a=3 -suffix=PivotRandom_C${i}_iteration${j} -m=1
 	done
 	cat Slashdot0902PivotRandom_C${i}_iteration*.time > Slashdot0902PivotRandom_C${i}.time
 	rm Slashdot0902PivotRandom_C${i}_iteration*.time

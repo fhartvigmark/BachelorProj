40105ac65aa427e0d9735aa03b7be3f4f28fc4ca
Eske <eskehoy@gmail.com>
2019-06-04 01:31:01 +0200

    Fixed mistake in benchmarking base job script.

--- jobs/benchmarking/test_FWBW_performance.sh
+++ jobs/benchmarking/test_FWBW_performance.sh
@@ -5,7 +5,7 @@ for j in 1 2 3 4 5 6 7 8 9 10
 do
 	./main -g=../graphs/${1}.edges -w=${i} -p=3 -time=3 -suffix=Baseline_C${i}_iteration${j} -m=0 -t=3 -cutoff=5 -steps=${2}
 done
-cat ${1}Baselin_C${i}_iteration*.time > ${1}Baseline_C${i}.time
+cat ${1}Baseline_C${i}_iteration*.time > ${1}Baseline_C${i}.time
 rm ${1}Baseline_C${i}_iteration*.time
 for i in 1 2 4 8 16 24 32 48
 do

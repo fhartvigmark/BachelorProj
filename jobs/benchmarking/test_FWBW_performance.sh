03b7183f3ba55b3ab5c706788c779b7c8796e7ca
Eske <eskehoy@gmail.com>
2019-06-04 00:34:13 +0200

    DynamicFWBW job script addition.

--- jobs/benchmarking/test_FWBW_performance.sh
+++ jobs/benchmarking/test_FWBW_performance.sh
@@ -25,3 +25,12 @@ do
 	cat ${1}RecFWBW_C${i}_iteration*.time > ${1}RecFWBW_C${i}.time
 	rm ${1}RecFWBW_C${i}_iteration*.time
 done
+for i in 1 2 4 8 16 24 32 48
+do
+	for j in 1 2 3 4 5 6 7 8 9 10
+	do
+		./main -g=../graphs/${1}.edges -w=${i} -p=3 -time=3 -suffix=DynamicFWBW_C${i}_iteration${j} -m=3 -t=3 -cutoff=5 -steps=${2}
+	done
+	cat ${1}DynamicFWBW_C${i}_iteration*.time > ${1}DynamicFWBW_C${i}.time
+	rm ${1}DynamicFWBW_C${i}_iteration*.time
+done

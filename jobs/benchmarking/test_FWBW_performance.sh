df3a4a17b990d57819512ad98c8de3620c8adde7
Eske <eskehoy@gmail.com>
2019-06-07 00:55:47 +0200

    Updated FWBW performance job script.

--- jobs/benchmarking/test_FWBW_performance.sh
+++ jobs/benchmarking/test_FWBW_performance.sh
@@ -7,10 +7,10 @@ for i in 1 2 4 8 16 32 64
 do
 	for j in 1 2 3 4 5 6 7 8 9 10
 	do
-		./main -g=../graphs/${1}.edges -w=1 -p=3 -time=3 -suffix=Baseline_iteration${j} -m=0 -t=3 -cutoff=5 -steps=${2}
+		./main -g=../graphs/${1}.edges -w=${i} -p=3 -time=3 -suffix=Baseline_C${i}_iteration${j} -m=0 -t=3 -cutoff=5 -steps=${2}
 	done
-	cat ${1}Baseline_iteration*.time > ${1}Baseline.time
-	rm ${1}Baseline_iteration*.time
+	cat ${1}Baseline_C${i}_iteration*.time > ${1}Baseline${i}.time
+	rm ${1}Baseline_C${i}_iteration*.time
 done
 for i in 1 2 4 8 16 32 64
 do

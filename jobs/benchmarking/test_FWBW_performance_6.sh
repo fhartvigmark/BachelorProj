eb112a9963893335017d7e492b86154e58dfb7c7
Eske <eskehoy@gmail.com>
2019-06-07 11:07:43 +0200

    Updated job scripts for pantents and wiki graphs.

--- jobs/benchmarking/test_FWBW_performance_6.sh
+++ jobs/benchmarking/test_FWBW_performance_6.sh
@@ -5,4 +5,4 @@
 #SBATCH -c64
 #SBATCH --exclusive
 #SBATCH -w weleveld
-srun -N1 -n1 --exclusive test_FWBW_performance.sh Patents 200
+srun -N1 -n1 --exclusive test_FWBW_performance_single.sh Patents 200
--- jobs/benchmarking/test_FWBW_performance_7.sh
+++ jobs/benchmarking/test_FWBW_performance_7.sh
@@ -5,4 +5,4 @@
 #SBATCH -c64
 #SBATCH --exclusive
 #SBATCH -w weleveld
-srun -N1 -n1 --exclusive test_FWBW_performance.sh Wiki 200
+srun -N1 -n1 --exclusive test_FWBW_performance_single.sh Wiki 200
